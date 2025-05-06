53f3feb19c43f1a4db1bc1861c247c12555c36eb
ltxlong <416803647@qq.com>
2025-05-06 22:51:10 +0800

    Update index.d.ts

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,8 +1,15 @@
 export interface ResourceSite {
     url: string;
-    className?: string;
+    searchResultClass?: string;
     remark: string;
     active: boolean;
+    isPost?: boolean;
+    postData?: string;
+    adFilter: {
+        status: boolean;
+        item: string;
+        regularExpression: string;
+    }
 }
 export interface Config {
     resourceSites: ResourceSite[];
@@ -12,4 +19,12 @@ export interface Config {
     loginPassword: string;
     announcement: string;
     customTitle: string;
+    enableHealthFilter: boolean;
+    proxyVideoUrl: string;
+    proxyLiveUrl: string;
+    enableHotMovies: boolean;
+    hotMoviesProxyUrl: string;
+    hotTvDefaultTag: string;
+    hotMovieDefaultTag: string;
+    autoPlayNext: boolean;
 }
