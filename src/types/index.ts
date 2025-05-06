5ef4c9111ddc4efb765edfe9ab9401edeb40759b
ltxlong <416803647@qq.com>
2025-05-06 22:56:07 +0800

    Update index.ts

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,11 +1,15 @@
 export interface ResourceSite {
   url: string
-  className?: string
   searchResultClass?: string
   remark: string
   active: boolean
   isPost?: boolean
   postData?: string
+  adFilter: {
+    status: boolean
+    item: string
+    regularExpression: string
+  }
 }
 
 export interface PublicConfig {
@@ -15,6 +19,14 @@ export interface PublicConfig {
   enableLogin: boolean
   announcement: string
   customTitle?: string | 'false' | false
+  enableHealthFilter: boolean
+  proxyVideoUrl: string
+  proxyLiveUrl: string
+  enableHotMovies?: boolean
+  hotMoviesProxyUrl?: string
+  hotTvDefaultTag?: string
+  hotMovieDefaultTag?: string
+  autoPlayNext?: boolean
 }
 
 export interface Config extends PublicConfig {
