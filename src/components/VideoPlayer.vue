39b08b6f9a9dbef1bca1ba891e6257a3b0af1175
ltxlong <416803647@qq.com>
2025-05-23 23:28:04 +0800

    Update VideoPlayer.vue

--- src/components/VideoPlayer.vue
+++ src/components/VideoPlayer.vue
@@ -1709,7 +1709,7 @@ function customLoaderFactory() {
   <iframe
     v-else
     ref="iframeContainer"
-    :src= "`/api/iframe-filter?url=${encodeURIComponent(getParseUrl(url))}`"
+    :src= "getParseUrl(url)"
     class="w-full aspect-video border-0"
     allowfullscreen
     frameborder="0"
